64d63e7ae96607e0365d607c2a32564ce77ab1d1
Matthew Lewis <mlewis1981@outlook.com>
2024-07-12 11:00:35 +0100

    default sep '  ' wordgen arguments configured

--- PassGenius-cli/Program.cs
+++ PassGenius-cli/Program.cs
@@ -4,54 +4,103 @@
     {
         static void Main(string[] args)
         {
-            // If 0 arguments passed display help
-            if (args.Length == 0)
+            if  (args.Length > 1 && args[0] == "p")
             {
-                DisplayHelp();
-            }
-            // If arguments are correct length and start with P Try and generate a password
-            if (args.Length == 3 && args[0] == "p") {
-
-                if (int.TryParse(args[1], out var length) && int.TryParse(args[2], out var set))
+                switch (args.Length)
                 {
-                    // User gave us vaild length and set we can proceed to generate password.
-                    Console.WriteLine(PasswordGenerator.GetPassword(length, set));
+                    case 3:
+                        if (int.TryParse(args[1], out var length) && int.TryParse(args[2], out var set)){
+                            Console.WriteLine(PasswordGenerator.GetPassword(length, set));
+                        }
+                        else
+                        {
+                            DisplayHelp();
+                        }
+                        break;
+                    default:
+                        DisplayHelp();
+                        break;
+                        
                 }
-                else
+               
+            }
+            else if (args.Length > 1 && args[0] == "w")
+            {
+                switch (args.Length)
                 {
-                    DisplayHelp();
-                }  
+                    case 4:
+                        if (int.TryParse(args[1], out var numOfWords) && char.TryParse(args[2], out var separatorCase4))
+                        {
+                            if (args[3].Equals("t", StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                Console.WriteLine(WordGenerator.GetWords(numOfWords, separatorCase4, enableTitleCase:true));
+                            }
+                            else if (args[3].Equals("u", StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                Console.WriteLine(WordGenerator.GetWords(numOfWords, separatorCase4, enableUpperCase: true));
+                            }
+                            else
+                            {
+                                DisplayHelp();
+                            }
+                        }
+                        break;
+                    case 3:
+                        if (int.TryParse(args[1], out var case3Words))
+                        {
+                            if (args[2].Equals("t", StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                Console.WriteLine(WordGenerator.GetWords(case3Words, enableTitleCase: true));
+                            }
+                            else if (args[2].Equals("u", StringComparison.CurrentCultureIgnoreCase))
+                            {
+                                Console.WriteLine(WordGenerator.GetWords(case3Words, enableUpperCase: true));
+                            }
+                            else if (char.TryParse(args[2], out var separatorCase3))
+                            {
+                                Console.WriteLine(WordGenerator.GetWords(case3Words, separator: separatorCase3));
+                            }
+                            else
+                            {
+                                DisplayHelp();
+                            }
+                        }
+                        break;
+                    case 2:
+                        if (int.TryParse(args[1], out var case2Words))
+                        {
+                            Console.WriteLine(WordGenerator.GetWords(case2Words));
+                        }
+                        break;
+                    default:
+                        DisplayHelp();
+                        break;
+                }
             }
             else
             {
                 DisplayHelp();
             }
-            
         }
 
 
         static void DisplayHelp()
         {
-            Console.WriteLine("\nUsage Password: passgenius [p] [length] [set]\r\n");
+            Console.WriteLine("\nUsage Password: passgenius-cli [p] [length] [set]");
+            Console.WriteLine("Usage Passphrase: PassGenius-cli [w] [NumberOfWords] [Separator] [U]uppercase || [T]titlecase\r\n");
             Console.WriteLine("""
                 Available character sets:
-                1  = Lower + Upper + Numbers + Special
-                2  = Lower + Upper + Numbers
-                3  = Lower + Upper + Special
-                4  = Lower + Upper
-                5  = Lower + Numbers + Special
-                6  = Lower + Numbers
-                7  = Lower + Special
-                8  = Lower
-                9  = Upper + Numbers + Special
-                10 = Upper + Numbers
-                11 = Upper + Special
-                12 = Upper
-                13 = Numbers + Special
-                14 = Numbers
-                15 = Special
+                1:  Lower + Upper + Numbers + Special | 2:  Lower + Upper + Numbers
+                3:  Lower + Upper + Special           | 4:  Lower + Upper
+                5:  Lower + Numbers + Special         | 6:  Lower + Numbers
+                7:  Lower + Special                   | 8:  Lower
+                9:  Upper + Numbers + Special         | 10: Upper + Numbers
+                11: Upper + Special                   | 12: Upper
+                13: Numbers + Special                 | 14: Numbers
+                15: Special
                 """);
-            Console.WriteLine("Example: passgenius p 14 1 (creates a password of 14 character made of Lower, Upper, Numbers and Special characters)");
+            Console.WriteLine("Ex Password: passgenius-cli p 14 1 (creates a password of 14 character made of Lower, Upper, Numbers and Special characters)");
+            Console.WriteLine("Example Passphrase: passgenius-cli w 4 U (creates GALLANTLY ANTHOLOGY LAZILY ENSLAVE )");
 
         }
     }
---
+++ PassGenius-cli/WordGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Text;
+
+public class WordGenerator
+{
+    public static string GetWords(int numberOfWords = 3, char separator = ' ', bool enableUpperCase = false, bool enableTitleCase = false)
+    {
+        Random random = new();
+        StringBuilder sb = new();
+        var count = WordList.EFFLongWordList.Count;
+        TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+        int luckyNumber = random.Next(numberOfWords);
+
+        List<string> words = [];
+        for (int i = 0; i < numberOfWords; i++)
+        {
+            var word = WordList.EFFLongWordList[random.Next(count)];
+            if (enableUpperCase && !enableTitleCase)
+            {
+                words.Add(word.ToUpper());
+            }
+            else if (enableTitleCase && !enableUpperCase)
+            {
+                words.Add(textInfo.ToTitleCase(word.ToLower()));
+            }
+            else
+            {
+                words.Add(word);
+            }
+        }
+        return sb.AppendJoin(separator, words).ToString();
+    }
+}
